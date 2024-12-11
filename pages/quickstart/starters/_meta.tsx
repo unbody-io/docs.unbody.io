8eededae581d9876049d0eae430091390f9e533c
Md. Azaz Ahmed Akib <33729375+akib001@users.noreply.github.com>
2024-12-12 00:25:02 +0600

    feat: added contents for setup overview

--- pages/quickstart/starters/_meta.tsx
+++ pages/quickstart/starters/_meta.tsx
@@ -1,9 +1,8 @@
 const meta = {
-  index: "Starters Overview",
   nextjs: "Next.js",
   expressjs: "Express.js",
   nestjs: "Nest.js",
-  nuxtjs: "Nuxt.js"
+  nuxtjs: "Nuxt.js",
 };
 
 export default meta;
