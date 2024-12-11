9ed1ccad43e9c67f7d4eb0b6c558a7ac3df7457b
Md. Azaz Ahmed Akib <33729375+akib001@users.noreply.github.com>
2024-12-12 02:45:12 +0600

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,16 +14,16 @@ export const Footer = () => {
 
         <div className="flex flex-col gap-2">
           <h3 className="font-semibold mb-2">Company</h3>
-          <a href="/about" className="hover:opacity-70">
+          <a href="https://unbody.io/about" className="hover:opacity-70">
             About
           </a>
-          <a href="/pricing" className="hover:opacity-70">
+          <a href="https://unbody.io/pricing" className="hover:opacity-70">
             Pricing
           </a>
-          <a href="/xbody" className="hover:opacity-70">
+          <a href="https://unbody.io/" className="hover:opacity-70">
             Xbody
           </a>
-          <a href="/faq" className="hover:opacity-70">
+          <a href="https://unbody.io/#card04" className="hover:opacity-70">
             FAQ
           </a>
         </div>
@@ -31,31 +31,35 @@ export const Footer = () => {
         <div className="flex flex-col gap-2">
           <h3 className="font-semibold mb-2">Contact</h3>
           <a
-            href="https://linkedin.com"
+            href="https://www.linkedin.com/company/unbody"
             target="_blank"
             className="hover:opacity-70"
           >
             LinkedIn
           </a>
-          <a href="https://x.com" target="_blank" className="hover:opacity-70">
+          <a
+            href="https://twitter.com/unbody_io"
+            target="_blank"
+            className="hover:opacity-70"
+          >
             X
           </a>
           <a
-            href="https://github.com"
+            href="https://github.com/unbody-io"
             target="_blank"
             className="hover:opacity-70"
           >
             Github
           </a>
           <a
-            href="https://youtube.com"
+            href="https://www.youtube.com/@Unbody"
             target="_blank"
             className="hover:opacity-70"
           >
             YouTube
           </a>
           <a
-            href="https://discord.com"
+            href="https://discord.gg/UX8WKEsVPu"
             target="_blank"
             className="hover:opacity-70"
           >
@@ -65,10 +69,10 @@ export const Footer = () => {
 
         <div className="flex flex-col gap-2">
           <h3 className="font-semibold mb-2">Resources</h3>
-          <a href="/documentation" className="hover:opacity-70">
+          <a href="/" className="hover:opacity-70">
             Documentation
           </a>
-          <a href="/blog" className="hover:opacity-70">
+          <a href="https://unbody.io/blog" className="hover:opacity-70">
             Blog
           </a>
         </div>
@@ -77,11 +81,11 @@ export const Footer = () => {
       <div className="flex justify-between pt-4 border-t border-[var(--logo-color)] opacity-70">
         <span>©2024 Unbody</span>
         <div className="flex gap-2">
-          <a href="/terms" className="hover:opacity-70">
+          <a href="https://unbody.io/terms" className="hover:opacity-70">
             Terms
           </a>
           <span>·</span>
-          <a href="/privacy" className="hover:opacity-70">
+          <a href="https://unbody.io/privacy" className="hover:opacity-70">
             Privacy
           </a>
         </div>
