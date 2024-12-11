682da3b16f10aa8f664cb90cc0e8622f93d1b5af
Md. Azaz Ahmed Akib <33729375+akib001@users.noreply.github.com>
2024-12-12 01:51:14 +0600

    feat: setup docs

---
+++ pages/backend-layer/_meta.tsx
@@ -0,0 +1,10 @@
+const meta = {
+ "overview": "Backend Layer Overview",
+ "data-ingestion": "Data Ingestion",
+ "data-processing": "Data Processing",
+ "data-enrichment": "Data Enrichment",
+ "data-vectorization": "Data Vectorization",
+ "cdn-and-caching": "CDN and Caching"
+};
+
+export default meta;
