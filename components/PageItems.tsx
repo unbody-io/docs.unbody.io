f7d36020fe124a6c43f57ad9751813a351d0cc56
Md. Azaz Ahmed Akib <33729375+akib001@users.noreply.github.com>
2024-12-12 18:01:09 +0600

    fix: build error

--- components/PageItems.tsx
+++ components/PageItems.tsx
@@ -1,3 +1,9 @@
-export const PageItems = ({ children }) => (
+import { ReactNode } from "react";
+
+interface PageItemsProps {
+  children: ReactNode;
+}
+
+export const PageItems: React.FC<PageItemsProps> = ({ children }) => (
   <div className={"grid lg:grid-cols-2 gap-6 mt-6 grid-cols-1"}>{children}</div>
 );
