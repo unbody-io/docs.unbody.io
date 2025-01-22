131a6ad11b0773f24a375d15f8dfd2c9a7700a88
Md. Azaz Ahmed Akib <33729375+akib001@users.noreply.github.com>
2025-01-22 22:53:34 +0600

    fix: change rag menus names

--- pages/generative-and-rag/_meta.tsx
+++ pages/generative-and-rag/_meta.tsx
@@ -1,9 +1,10 @@
 const meta = {
   "text-generation": "Text generation",
   json: "JSON",
-  "basic-using-generative-search": "Basic Using Generative Search",
-  "multimodal-using-generative-search": "Multimodal Using Generative Search",
-  "multimodal-using-generative-api": "Multimodal Using Generative API",
+  "simple-rag-with-generative-search": "Simple Rag With Generative Search",
+  "multimodal-rag-using-generative-search":
+    "Multimodal Rag Using Generative Search",
+  "multimodal-rag-using-generative-api": "Multimodal Rag Using Generative API",
 };
 
 export default meta;
