af826eff28eec416360305417bb48078209bdfcc
Md. Azaz Ahmed Akib <33729375+akib001@users.noreply.github.com>
2025-01-22 19:45:05 +0600

    add images, multimodal

--- pages/generative-and-rag/_meta.tsx
+++ pages/generative-and-rag/_meta.tsx
@@ -1,6 +1,9 @@
 const meta = {
+  "text-generation": "Text generation",
   json: "JSON",
   "basic-using-generative-search": "Basic Using Generative Search",
+  "multimodal-using-generative-search": "Multimodal Using Generative Search",
+  "multimodal-using-generative-api": "Multimodal Using Generative API",
 };
 
 export default meta;
