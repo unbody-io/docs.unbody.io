7298cf94cb9d9be6d9216134128d0de0296412de
amirhouieh <amir.houieh@gmail.com>
2025-05-26 12:48:41 +0200

    add streaming support

--- pages/generative-and-rag/_meta.tsx
+++ pages/generative-and-rag/_meta.tsx
@@ -1,6 +1,7 @@
 const meta = {
   text: "Text",
   json: "JSON",
+  streaming: "Streaming",
   "rag-with-generative-search": "Rag With Generative Search",
   "multimodal-rag-using-generative-search":
     "Multimodal Rag Using Generative Search",
